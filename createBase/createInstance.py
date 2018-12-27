2db3ae5f5e9adbc81eefb2cf38232e0e8113d61b
FelixNeoAA <felixneoaa@gmail.com>
2018-12-27 22:21:02 +0100

    data.txt

--- createBase/createInstance.py
+++ createBase/createInstance.py
@@ -5,6 +5,8 @@ def CreateInstance(numJobs, numMachine):
     ptime = np.random.randint(10, 100, size=[numJobs, numMachine])
     ids = np.arange(1, numJobs + 1, 1, dtype=np.int64).reshape(numJobs, 1)
     instance = np.append(ids, ptime, axis=1)
-    np.savetxt('/Users/alafateabulimiti/PycharmProjects/PRD/instances/data.txt', instance, fmt='%d %d %d',
+    np.savetxt('C:/Users/Alafate ABULIMITI/Documents/PRD/instances/data.txt', instance, fmt='%d %d %d',
                delimiter=' ')
     return instance
+
+CreateInstance(20,2)
