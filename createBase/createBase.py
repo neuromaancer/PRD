4212ce29c0a4f4e14b2f099b7d9055c7d0ff3a60
“AlafateABULIMITI” <felixneoaa@gmail.com>
2018-12-29 12:27:33 +0100

    createRbsFile: it can convert seq file generated by matho to the rbs file, so that we can reuse this file to 2ed matho.

--- createBase/createBase.py
+++ createBase/createBase.py
@@ -1,8 +1,13 @@
 import numpy as np
 import os
+import shutil
+
 
 def createOptSeq(filePath):
     seed = 3  # random seed for the matheristic program
     tlim = 10000  # limited time for the matheristic program
     os.system("createRbsInstance.sh" + " " + filePath + " " + str(seed) + " " + str(tlim))
+
+def createRbsfile(seqFile, numIt):
+    shutil.copy(seqFile, str(numIt) + '_' + seqFile[:-3] + "rbs")
 
