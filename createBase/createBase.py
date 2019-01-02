eef0148912aa32ab63c708566339ab7f363671db
“AlafateABULIMITI” <felixneoaa@gmail.com>
2019-01-02 17:54:03 +0100

    add some support functions

--- createBase/createBase.py
+++ createBase/createBase.py
@@ -2,6 +2,7 @@ import numpy as np
 import os, sys
 import shutil
 import time
+import linecache
 from flowshop import compareSeq
 
 
@@ -20,11 +21,16 @@ def exeTime(func):
 def createOptSeq(filePath, seed, tlim):
     os.system("/Users/alafateabulimiti/PycharmProjects/PRD/createBase/F2sumCj.sh" + " " + filePath + " " + str(
         seed) + " " + str(tlim))
+    return filePath + ".seq"
 
 
 @exeTime
 def createRbsFile(seqFile, numIt):
-    newFile = 'It_' + str(numIt) + '_' + seqFile[:-3] + "rbs"
+    if seqFile[0:2] == "It":
+        newFile = 'It_' + str(numIt) + '_' + seqFile[:-3][5:] + "rbs"
+    else:
+        newFile = 'It_' + str(numIt) + '_' + seqFile[:-3] + "rbs"
+
     shutil.copy(seqFile, newFile)
     r = open(newFile)
     lines = r.readlines()
@@ -38,13 +44,11 @@ def createRbsFile(seqFile, numIt):
 
 def findOrigin(rbsFileWithIter):
     orginInstance = rbsFileWithIter[5:][:-4]
-    print(orginInstance)
     return orginInstance
 
 
 def createtxtFile(rbsFileWithIter):
     seq = np.loadtxt(rbsFileWithIter, dtype=int)[1:]
-    print(seq)
 
     orgintxt = findOrigin(rbsFileWithIter)
     orgintxtArr = np.loadtxt(orgintxt, dtype=int)
@@ -57,26 +61,48 @@ def createtxtFile(rbsFileWithIter):
     ids = np.arange(1, numJobs + 1, 1, dtype=np.int64).reshape(numJobs, 1)
     instance = np.append(ids, ptimes, axis=1)
     instance = np.delete(instance, 1, axis=1)
-    np.savetxt(rbsFileWithIter[:-4], instance, fmt='%d %d %d', delimiter=' ')
+    filename = rbsFileWithIter[:-4]
+    np.savetxt(filename, instance, fmt='%d %d %d', delimiter=' ')
+
+    return filename
 
 
-def calculateI(baseSeqFile, targetSeqFile):
-    _, _, baseFO, targetFO = compareSeq(baseSeqFile, targetSeqFile)
+def calculateI(baseFO, targetFO):
     improvement = 1 - (targetFO / baseFO)
     return improvement
 
 
-def generateBase(exTime, numIt, instancePath):
+def extractFOfromFile(file):
+    f = open(file)
+    rbs = f.readline().split(" ")
+    initFO = int(rbs[0])
+    return initFO
+
+
+def extractRH(seqfile):
+    rh = linecache.getline(seqfile, 2).split(" ")
+    r = rh[0]
+    h = rh[1]
+    return r, h
+
+
+def generateBaseForOneIns(exTime, numIt, instanceFile):
     begin = time.clock()
     end = 0
-    num = 0
+    num = 1
+    inirbsFile = instanceFile + ".rbs"
+    initFO = extractFOfromFile(inirbsFile)
+    newinstancefile = instanceFile
     while end < exTime and num < numIt:
-        files = os.listdir(instancePath)
-        for file in files:
-            if file.endswith(".txt"):
-                createOptSeq(instancePath + "/" + file, 3, 100)
-                createRbsFile(file+".seq",)
-
+        seqfile = createOptSeq(newinstancefile, 3, 10)
+        rbsfile = createRbsFile(seqfile, num)
+        optFO = extractFOfromFile(seqfile)
+        rbsFO = extractFOfromFile(rbsfile)
+        I = calculateI(initFO, rbsFO)
+        Iprime = calculateI(initFO, optFO)
+        r, h = extractRH(seqfile)
+        newinstancefile = createtxtFile(rbsfile)
+        print(r, h, I, Iprime)
         current = time.clock()
         end = current - begin
         num = num + 1
@@ -85,4 +111,7 @@ def generateBase(exTime, numIt, instancePath):
 if __name__ == "__main__":
     # generateBase(1000, 1, '/Users/alafateabulimiti/PycharmProjects/PRD/createBase')
     # createOptSeq("/Users/alafateabulimiti/PycharmProjects/PRD/createBase/data.txt",3,10)
-    createtxtFile("It_2_data.txt.rbs")
+    # createtxtFile("It_2_data.txt.rbs")
+    # createOptSeq("data.txt", 3, 100)
+    generateBaseForOneIns(300, 3, "data.txt")
+    # createRbsFile("It_1_data.txt.seq", 2)
